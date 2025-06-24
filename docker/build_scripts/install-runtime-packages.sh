190b88658791681ee5e54152c1d3c314e88337e1
Dustin Rodrigues <dust.rod@gmail.com>
2025-06-23 21:49:06 -0400

    install findutils

--- docker/build_scripts/install-runtime-packages.sh
+++ docker/build_scripts/install-runtime-packages.sh
@@ -116,7 +116,7 @@ if [ "${AUDITWHEEL_POLICY}" == "manylinux2014" ]; then
 	fi
 	fixup-mirrors
 elif [ "${OS_ID_LIKE}" == "rhel" ]; then
-	BASE_TOOLS+=(glibc-locale-source glibc-langpack-en hardlink hostname libcurl libnsl libxcrypt which)
+	BASE_TOOLS+=(glibc-locale-source glibc-langpack-en hardlink hostname libcurl libnsl libxcrypt which findutils)
 	echo "tsflags=nodocs" >> /etc/dnf/dnf.conf
 	dnf -y upgrade
 	EPEL=epel-release
